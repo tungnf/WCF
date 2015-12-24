17a0f0f1fee70995262512be60ab1d4081052b8a
tungnf <tungnf@gmail.com>
2015-12-24 19:20:02 +0800

    Initial Commit
    Initial Commit

---
+++ MyMSMQClient/MyMSMQClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyMSMQClient.MyMSMQService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMSMQClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MsmqContractClient client = new MsmqContractClient();
+            client.SendMessage("Hello World!! 1234");
+            client.SendMessage("Hello World!! aaaa");
+            client.SendMessage("Hello World!! bbbb");
+            client.SendMessage("Hello World!! cccc");
+            client.Close();
+        }
+    }
+}
